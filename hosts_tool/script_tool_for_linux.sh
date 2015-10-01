1003aeff3042b5356887090f82d9bc4c90582476
Kevin Guan <KevinGuan.gm@gmail.com>
2015-10-02 07:57:39 +0800

    fix some bug and this version can auto change the hostname

--- hosts_tool/script_tool_for_linux.sh
+++ hosts_tool/script_tool_for_linux.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 if [ ${UID} != 0 ]; then
-    cp /etc/hosts ./hosts-$(date +%F-%H%M%S).bak
-    wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O ./fetchedhosts
+    mv /etc/hosts /etc/hosts-$(date +%F-%H%M%S).bak
+    wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O fetchedhosts
+	sed -i "s/localhost/localhost $(hostname)/g" fetchedhosts
+	sed -i "s/broadcasthost/broadcasthost $(hostname)/g" fetchedhosts
 
-    echo ./fetchedhosts >> /etc/hosts
+    cat fetchedhosts >> /etc/hosts
     echo Success.
 else
     echo permission denied, are you root?

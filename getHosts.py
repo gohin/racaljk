5c4f0d973d9a3f104a96f6b441b6b44a734fb8c9
Kevin Guan <KevinGuan.gm@gmail.com>
2015-10-02 07:57:05 +0800

    list comprehension is so good

--- getHosts.py
+++ getHosts.py
@@ -28,40 +28,14 @@ class hosts(object):
             f.write(content)
 
     def printContributors(self):
-        c = [
-            {
-                'author'     : 'racaljk',
-                'githubpage' :'https://github.com/racaljk'
-            },
-            {
-                'author'     : 'andytimes',
-                'githubpage' : 'https://github.com/andytimes'
-            },
-            {
-                'author'     : 'smounives',
-                'githubpage' : 'https://github.com/smounives'
-            },
-            {
-                'author'     : 'cw881014',
-                'githubpage' : 'https://github.com/cw881014'
-            },
-            {
-                'author'     : 'TimothyGu',
-                'githubpage' : 'https://github.com/TimothyGu'
-            },
-            {
-                'author'     : 'RellyVadd',
-                'githubpage' : 'https://github.com/RellyVadd'
-            },
-            {
-                'author'     : 'LyricTian',
-                'githubpage' : 'https://github.com/LyricTian'
-            }
-        ]
+        l = ['racaljk', 'andytimes', 'smounives', 'smounives',
+             'TimothyGu', 'RellyVadd', 'LyricTian']
+
+        c = []
+        [c.append({'author': i,
+                   'githubpage': 'https://github.com/'+i}) for i in l]
+
         print ':D thanks for all contributors:'
         for x in xrange(len(c)):
             for (k,v) in c[x].items():
                 print k+':'+v
-
-
-

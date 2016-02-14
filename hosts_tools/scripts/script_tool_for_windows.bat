be733e73205b9041bb7bd064de0fa90af675180b
lideming <910962400@qq.com>
2016-02-15 04:01:16 +0800

    Improve HostsGeter.bat

--- hosts_tools/scripts/script_tool_for_windows.bat
+++ hosts_tools/scripts/script_tool_for_windows.bat
@@ -1,54 +1,88 @@
-
-
+@echo off
+cls
 %1 %2
-ver|find "5.">nul&&goto :st
+ver|find " 5.">nul &&goto :st
+echo 正在进行 UAC 提权...
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :st","","runas",1)(window.close)&goto :eof
 :st
 
 copy "%~0" "%windir%\system32\"
 cls
-@REM HostsGet Version0.3.1
-
 @echo off
-del %temp%\download.vbs > nul
+@REM HostsGet Version0.4
 cd /d %~dp0
-echo 初始化中...
 
-echo iLocal=LCase("c:\hosts") >> %temp%\download.vbs
-echo iRemote=LCase("https://raw.githubusercontent.com/racaljk/hosts/master/hosts") >> %temp%\download.vbs
+set LogFile=%temp%\HostsGeter_log.txt
+set LogToFile=^>^>%LogFile% 2^>^&1
+set EchoAndLog=call :echoandlog
+echo. %LogToFile%
+echo ==========[%date% %time%]========== %LogToFile%
+echo 日志文件：
+echo %LogFile%
+echo.
 
-echo Set xPost=createObject("Microsoft.XMLHTTP") 'Set Post = CreateObject("Msxml2.XMLHTTP") >> %temp%\download.vbs
-echo xPost.Open "GET",iRemote,0 >> %temp%\download.vbs
-echo xPost.Send() >> %temp%\download.vbs
-echo set sGet=createObject("ADODB.Stream") >> %temp%\download.vbs
-echo sGet.Mode=3 >> %temp%\download.vbs
-echo sGet.Type=1 >> %temp%\download.vbs
-echo sGet.Open() >> %temp%\download.vbs
-echo sGet.Write xPost.ResponseBody >> %temp%\download.vbs
-echo sGet.SaveToFile iLocal,2 >> %temp%\download.vbs
+set dlvbs=%temp%\downloadhosts.vbs
+echo iLocal=LCase("c:\hosts") > %temp%\downloadhosts.vbs ||(
+ call :error downloadhosts.vbs 文件创建/写入失败.
+)
+echo iRemote=LCase("https://raw.githubusercontent.com/racaljk/hosts/master/hosts") >> %temp%\downloadhosts.vbs
+echo Set xPost=createObject("Microsoft.XMLHTTP") 'Set Post = CreateObject("Msxml2.XMLHTTP") >> %temp%\downloadhosts.vbs
+echo xPost.Open "GET",iRemote,0 >> %temp%\downloadhosts.vbs
+echo xPost.Send() >> %temp%\downloadhosts.vbs
+echo set sGet=createObject("ADODB.Stream") >> %temp%\downloadhosts.vbs
+echo sGet.Mode=3 >> %temp%\downloadhosts.vbs
+echo sGet.Type=1 >> %temp%\downloadhosts.vbs
+echo sGet.Open() >> %temp%\downloadhosts.vbs
+echo sGet.Write xPost.ResponseBody >> %temp%\downloadhosts.vbs
+echo sGet.SaveToFile iLocal,2 >> %temp%\downloadhosts.vbs
 
-echo 初始化完成...请稍后...
-ping 127.0.0.1 > nul
-echo 开始从github中下载hosts
-%temp%\download.vbs
-echo 请稍后...
-del %temp%\download.vbs /s /q
-ping 127.0.0.1 > nul
-echo 正在备份原hosts...
-move %windir%\system32\drivers\etc\hosts %windir%\
-set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-ren %windir%\hosts %filename%.bak
+%EchoAndLog% 开始从 GitHub 中下载 hosts 文件...
+%temp%\downloadhosts.vbs || call :error hosts 文件下载失败.
+del %temp%\downloadhosts.vbs /s /q %LogToFile%
+%EchoAndLog% 下载完成.
+echo.
 
+if exist %windir%\system32\drivers\etc\hosts (
+%EchoAndLog% 正在备份原 hosts 文件...
+set filename=hosts_%random%.bak
+copy %windir%\system32\drivers\etc\hosts %windir%\%filename% %LogToFile% || call :error hosts 文件备份失败.
+%EchoAndLog% 原 hosts 文件已备份到 %windir%\%filename%.
+echo.
+) else (
+%EchoAndLog% （原 hosts 文件不存在，跳过备份）
+)
+%EchoAndLog% 正在替换 hosts 文件...
+move c:\hosts %windir%\system32\drivers\etc\ %LogToFile% || call :error hosts 文件替换失败.
+%EchoAndLog% hosts 文件已替换.
+echo.
 
-echo hosts文件已经备份到%windir%，名字为%filename% ，路径为：%windir%\%filename%  .
+%EchoAndLog% 正在刷新系统 DNS 缓存...
+ipconfig /flushdns %LogToFile% || call :error DNS 缓存刷新失败.
+%EchoAndLog% DNS 缓存已刷新.
+echo.
+%EchoAndLog% 【操作全部完成！】
+echo.
 
-move c:\hosts %windir%\system32\drivers\etc\
-echo hosts替换完成.
-ipconfig /flushdns
-echo 按任意键访问google.com.hk进行测试，如取消，请直接关闭本窗口
+echo 按任意键访问 google.com.hk 进行测试，如取消，请直接关闭本窗口
 pause >nul
 start https://www.google.com.hk
-echo 已经帮你访问google.com.hk 如可以访问则替换成功.
+echo 已经帮你访问 google.com.hk 如可以访问则替换成功.
+echo.
+goto :end
 
+:error
+echo ======================
+%EchoAndLog% 错误：%*
+start %LogFile%
+echo 已打开日志文件
+goto :end
+
+:echoandlog
+echo %*
+echo %* %LogToFile%
+goto :eof
+
+:end
 echo 按任意键关闭
 pause >nul
+exit

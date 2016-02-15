945c0ed7ce8d118ea7f9629a96e97c39540dec73
lideming <910962400@qq.com>
2016-02-15 21:06:29 +0800

    Update and move HostsGeter

--- hosts_tools/scripts/script_tool_for_windows.bat
+++ hosts_tools/scripts/script_tool_for_windows.bat
@@ -8,51 +8,53 @@ mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :st",
 
 copy "%~0" "%windir%\system32\"
 cls
-@echo off
+::@echo off
 @REM HostsGet Version0.4
 cd /d %~dp0
 
-set LogFile=%temp%\HostsGeter_log.txt
-set LogToFile=^>^>%LogFile% 2^>^&1
+set LogFilePath=%temp%\HostsGeter_log.txt
+set DLScriptPath=%temp%\downloadhosts.vbs
+set DLPath=%windir%\system32\drivers\etc\hosts_downloaded
+set BackupDir=%windir%\system32\drivers\etc
+set HostsPath=%windir%\system32\drivers\etc\hosts
+
+set LogToFile=^>^>%LogFilePath% 2^>^&1
 set EchoAndLog=call :echoandlog
 echo. %LogToFile%
 echo ==========[%date% %time%]========== %LogToFile%
 echo 日志文件：
-echo %LogFile%
+echo %LogFilePath%
 echo.
 
-set dlvbs=%temp%\downloadhosts.vbs
-echo iLocal=LCase("c:\hosts") > %temp%\downloadhosts.vbs ||(
+echo iLocal=LCase("%DLPath%") > %DLScriptPath% ||(
  call :error downloadhosts.vbs 文件创建/写入失败.
 )
-echo iRemote=LCase("https://raw.githubusercontent.com/racaljk/hosts/master/hosts") >> %temp%\downloadhosts.vbs
-echo Set xPost=createObject("Microsoft.XMLHTTP") 'Set Post = CreateObject("Msxml2.XMLHTTP") >> %temp%\downloadhosts.vbs
-echo xPost.Open "GET",iRemote,0 >> %temp%\downloadhosts.vbs
-echo xPost.Send() >> %temp%\downloadhosts.vbs
-echo set sGet=createObject("ADODB.Stream") >> %temp%\downloadhosts.vbs
-echo sGet.Mode=3 >> %temp%\downloadhosts.vbs
-echo sGet.Type=1 >> %temp%\downloadhosts.vbs
-echo sGet.Open() >> %temp%\downloadhosts.vbs
-echo sGet.Write xPost.ResponseBody >> %temp%\downloadhosts.vbs
-echo sGet.SaveToFile iLocal,2 >> %temp%\downloadhosts.vbs
+echo iRemote=LCase("https://raw.githubusercontent.com/racaljk/hosts/master/hosts") >> %DLScriptPath%
+echo Set xPost=createObject("Microsoft.XMLHTTP") 'Set Post = CreateObject("Msxml2.XMLHTTP") >> %DLScriptPath%
+echo xPost.Open "GET",iRemote,0 >> %DLScriptPath%
+echo xPost.Send() >> %DLScriptPath%
+echo set sGet=createObject("ADODB.Stream") >> %DLScriptPath%
+echo sGet.Mode=3 >> %DLScriptPath%
+echo sGet.Type=1 >> %DLScriptPath%
+echo sGet.Open() >> %DLScriptPath%
+echo sGet.Write xPost.ResponseBody >> %DLScriptPath%
+echo sGet.SaveToFile iLocal,2 >> %DLScriptPath%
 
 %EchoAndLog% 开始从 GitHub 中下载 hosts 文件...
-%temp%\downloadhosts.vbs || call :error hosts 文件下载失败.
-del %temp%\downloadhosts.vbs /s /q %LogToFile%
+if exist %DLPath% del %DLPath% /s /q %LogToFile%
+%DLScriptPath% || call :error hosts 文件下载失败.
+del %DLScriptPath% /s /q %LogToFile%
+if not exist %DLPath% call :error hosts 文件下载失败.
 %EchoAndLog% 下载完成.
 echo.
 
-if exist %windir%\system32\drivers\etc\hosts (
-%EchoAndLog% 正在备份原 hosts 文件...
-set filename=hosts_%random%.bak
-copy %windir%\system32\drivers\etc\hosts %windir%\%filename% %LogToFile% || call :error hosts 文件备份失败.
-%EchoAndLog% 原 hosts 文件已备份到 %windir%\%filename%.
-echo.
+if exist %HostsPath% (
+    call :backuphosts
 ) else (
-%EchoAndLog% （原 hosts 文件不存在，跳过备份）
+    %EchoAndLog% （原 hosts 文件不存在，跳过备份）
 )
 %EchoAndLog% 正在替换 hosts 文件...
-move c:\hosts %windir%\system32\drivers\etc\ %LogToFile% || call :error hosts 文件替换失败.
+move %DLPath% %HostsPath% %LogToFile% || call :error hosts 文件替换失败.
 %EchoAndLog% hosts 文件已替换.
 echo.
 
@@ -70,10 +72,22 @@ echo 已经帮你访问 google.com.hk 如可以访问则替换成功.
 echo.
 goto :end
 
+:backuphosts
+%EchoAndLog% 正在备份原 hosts 文件...
+set "bakfilename=hosts_%date%_%time:~0,8%.bak"
+set bakfilename=%bakfilename:/=-%
+set bakfilename=%bakfilename:\=-%
+set bakfilename=%bakfilename::=-%
+set bakfilename=%bakfilename: =_%
+copy %HostsPath% %BackupDir%\%bakfilename% %LogToFile% || call :error hosts 文件备份失败.
+%EchoAndLog% 原 hosts 文件已备份到 %BackupDir%\%bakfilename%.
+echo.
+goto :eof
+
 :error
 echo ======================
 %EchoAndLog% 错误：%*
-start %LogFile%
+start %LogFilePath%
 echo 已打开日志文件
 goto :end
 

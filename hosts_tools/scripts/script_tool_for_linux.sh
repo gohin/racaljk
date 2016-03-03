feb2d93331cb84b0a545c92dd45d0fb83301a2db
Andy Deng <theandy.deng@gmail.com>
2016-03-04 01:07:38 +0800

    script: linux: 更新 linux 脚本
    1) 修改 if 判断条件
    2) 使用此脚本时，仅更新本项目的规则及注释，保留其他内容。
    
    若 /etc/hosts 的其他内容中，含有别处的 hosts 规则，使用此脚本可能
    会产生冲突 （如：IP 规则失效），请手动替换。
    
    Signed-off-by: Andy Deng <theandy.deng@gmail.com>

--- hosts_tools/scripts/script_tool_for_linux.sh
+++ hosts_tools/scripts/script_tool_for_linux.sh
@@ -1,13 +1,20 @@
-#!/usr/bin/env bash
-if [ ${UID} == 0 ]; then
-    mv /etc/hosts /etc/hosts-$(date +%F-%H%M%S).bak
-    wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O fetchedhosts
-
-	sed -i "s/localhost/localhost $(hostname)/g" fetchedhosts
-	sed -i "s/broadcasthost/broadcasthost $(hostname)/g" fetchedhosts
-
-    mv fetchedhosts /etc/hosts
+#!/bin/sh
+#
+# script_tool_for_linux
+#
+# Using command: `sudo sh script_tool_for_linux.sh` or
+#                `su -c 'sh script_tool_for_linux.sh'`
+# to update your hosts file.
+#
+# @WARNING: the script CAN NOT replace other hosts rules.
+#           If you have others provided hosts rules, maybe get conflict.
+#
+if [ `id -u` -eq 0 ]; then
+    wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O ~/fetchedhosts
+    sed -i '/# Copyright (c) 2014/,/# Modified hosts end/d' /etc/hosts
+    cat ~/fetchedhosts >> /etc/hosts
+    rm -f ~/fetchedhosts
     echo Success.
 else
-    echo permission denied, are you root?
+    echo Permission denied, are you root?
 fi

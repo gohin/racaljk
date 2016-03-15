6ab6e66d3c13ec65ff9d7861cab23b29f8ffefc5
Kevin Guan <KevinGuan.gm@gmail.com>
2016-03-15 14:36:16 +0800

    Let's solve the #284

--- hosts_tools/scripts/script_tool_for_linux.sh
+++ hosts_tools/scripts/script_tool_for_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # script_tool_for_linux
 #
@@ -9,12 +9,17 @@
 # @WARNING: the script CAN NOT replace other hosts rules.
 #           If you have others provided hosts rules, maybe get conflict.
 #
-if [ `id -u` -eq 0 ]; then
-    wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O ~/fetchedhosts
+if [ ${UID} == 0 ]; then
+    wget 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts' -O /tmp/fetchedhosts
     sed -i '/# Copyright (c) 2014/,/# Modified hosts end/d' /etc/hosts
-    cat ~/fetchedhosts >> /etc/hosts
-    rm -f ~/fetchedhosts
-    echo Success.
+
+    sed -i '/127.0.0.1/d' /etc/hosts
+    echo "127.0.0.1 $(hostname)" >> /etc/hosts
+
+    cat /tmp/fetchedhosts >> /etc/hosts
+    rm -f /tmp/fetchedhosts
+
+    echo 'Success.'
 else
-    echo Permission denied, are you root?
+    echo 'Permission denied, are you root?'
 fi

380ca4289f5e97d7712a9cffbeb7493b17be24d0
SCaffrey <sirius.caffrey@gmail.com>
2016-03-30 23:37:17 +0800

    Update script. Solves #313, #284. Thanks to #285.

--- hosts_tools/scripts/script_tool_for_linux.sh
+++ hosts_tools/scripts/script_tool_for_linux.sh
@@ -2,19 +2,18 @@
 #
 # script_tool_for_linux
 #
-# Using command: `sudo sh script_tool_for_linux.sh` or
+# Use command: `sudo sh script_tool_for_linux.sh` or
 #                `su -c 'sh script_tool_for_linux.sh'`
 # to update your hosts file.
 #
-# @WARNING: the script CAN NOT replace other hosts rules.
-#           If you have others provided hosts rules, maybe get conflict.
+# @WARNING: the script CAN NOT replace others' hosts rules.
+#           If you have hosts rules provided by others, you may get conflict.
 #
 if [ `id -u` -eq 0 ]; then
     wget 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts' -O /tmp/fetchedhosts
     sed -i '/# Copyright (c) 2014/,/# Modified hosts end/d' /etc/hosts
 
-    sed -i '/127.0.0.1/d' /etc/hosts
-    echo "127.0.0.1 $(hostname)" >> /etc/hosts
+    sed -i '/127.0.0.1/c\127.0.0.1 '"$(hostname)"'\' /tmp/fetchedhosts
 
     cat /tmp/fetchedhosts >> /etc/hosts
     rm -f /tmp/fetchedhosts

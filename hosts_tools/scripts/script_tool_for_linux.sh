58410c524240b59e1cf4360402f9c3f8ef40b78a
Andy Deng <theandy.deng@gmail.com>
2016-04-11 13:25:38 +0800

    scripts: linux: update hostname solution

--- hosts_tools/scripts/script_tool_for_linux.sh
+++ hosts_tools/scripts/script_tool_for_linux.sh
@@ -6,14 +6,14 @@
 #                `su -c 'sh script_tool_for_linux.sh'`
 # to update your hosts file.
 #
-# @WARNING: the script CAN NOT replace others' hosts rules.
+# WARNING: the script CAN NOT replace others' hosts rules.
 #           If you have hosts rules provided by others, you may get conflict.
 #
 if [ `id -u` -eq 0 ]; then
     wget 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts' -O /tmp/fetchedhosts --no-check-certificate
     sed -i '/# Copyright (c) 2014/,/# Modified hosts end/d' /etc/hosts
 
-    sed -i '/127.0.0.1/c\127.0.0.1 '"$(hostname)"'\' /tmp/fetchedhosts
+    sed -i "s/localhost/`hostname`/g" /tmp/fetchedhosts
 
     cat /tmp/fetchedhosts >> /etc/hosts
     rm -f /tmp/fetchedhosts
